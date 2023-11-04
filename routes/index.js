6351ad7edc98cf43352ef6ccbcfbd24fed8246c2
angdembe_sona <asonaxaviersm1@gmail.com>
2023-11-04 12:25:47 +0545

    delete option is updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var books = require('../resources/books');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express sona' });
+  res.render('index', { title: 'Express sona' , bookList: books});
 });
 
 module.exports = router;
